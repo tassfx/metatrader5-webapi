61dedacb43b74a297868619dd7dbfff19e973900
crystaldaking <dev@tass.by>
2021-07-09 11:56:32 +0300

    Auth

--- src/Objects/Mt5Client.php
+++ src/Objects/Mt5Client.php
@@ -10,10 +10,82 @@ class Mt5Client
     private Client $client;
 
     //Params
-    private $ip;
-    private $port;
-    private $login;
-    private $password;
-    private $agent;
-    private $build;
+    private string $ip;
+    private int $port;
+    private int $login;
+    private string $password;
+    private string $agent;
+    private int $build;
+
+    private function auth()
+    {
+        if (!$this->client instanceof Client)
+        {
+            return $this->fail('Generic error', 'Guzzle is not initialized! R u ok?');
+        }
+
+        $params = [
+            'version' => $this->build,
+            'agent' => $this->agent,
+            'login' => $this->login,
+            'type' => 'manager'
+        ];
+
+        $result = $this->client->get('/auth_start', ['query' => $params]);
+
+        if ($result->getStatusCode() != 200)
+        {
+            return $this->fail('Generic error', $result->getStatusCode());
+        }
+
+        $result = json_decode($result->getBody(), true);
+
+        if ($result['retcode'] != '0 Done')
+        {
+            return $this->fail($result['retcode'],$result['answer']);
+        }
+
+        /**
+         * MT5 Psyhodelic
+         */
+        $srv_rand = hex2bin($result['srv_rand']);
+        $password_hash = md5(mb_convert_encoding($this->password, 'utf-16le', 'utf-8'), true) . 'WebAPI';
+        $srv_rand_answer = md5(md5($password_hash, true) . $srv_rand);
+        $cli_rand_buf = random_bytes(16);
+        $cli_rand = bin2hex($cli_rand_buf);
+
+        /**
+         * Send anwser
+         */
+        $params = [
+            'srv_rand_answer' => $srv_rand_answer,
+            'cli_rand' => $cli_rand
+        ];
+
+        $result = $this->client->get('/auth_answer', ['query' => $params]);
+
+        if ($result->getStatusCode() != 200)
+        {
+            return $this->fail('Generic error', $result->getStatusCode());
+        }
+
+        $result = json_decode($result->getBody(), true);
+
+        if ($result['retcode'] != '0 Done')
+        {
+            return $this->fail($result['retcode'],$result['answer']);
+        }
+
+        /**
+         * Calculating the correct server response for a random client sequence
+         */
+        $cliRandAnswer = md5(md5($password_hash, true) . $cli_rand_buf);
+
+        if ($cliRandAnswer != $result['cli_rand_answer'])
+        {
+            return $this->fail('Generic error', 'Auth answer error: rand buffs missmatch');
+        }
+
+        return $this->success('0 Done','OK!');
+    }
 }
